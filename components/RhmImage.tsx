0b591195c3b3ae09863eaaa7b3161d1bcb562257
Rob Maurizi <5453956+robmaurizi@users.noreply.github.com>
2022-12-01 17:54:28 -0500

    stripg #! attributes from img src

--- components/RhmImage.tsx
+++ components/RhmImage.tsx
@@ -11,15 +11,16 @@ type RhmImageProps = {
 const RhmImage = ({src, alt}: RhmImageProps) => {
   // const { src, alt } = props;
   if (!src || !alt) return <></>;
-  
+
   let imgAtts = src.substring(src.indexOf('#!')).replace('#!','');
+  const imgSrc = src.substring(0, src.indexOf('#!'));
   const attributes = Object.fromEntries(imgAtts.split('&').map(att => att.split('=') ));
   // console.log(attributes);
   const _classname = attributes.align ? `align_${attributes.align}` : '';
   // console.log(_classname);
   return (
     <figure className={styles[_classname]}>
-      <Image src={ src } alt={ alt } width={attributes.width} height={attributes.height} />
+      <Image src={ imgSrc } alt={ alt } width={attributes.width} height={attributes.height} />
       <figcaption className={styles.figCaption}>{ alt }</figcaption>
     </figure>
   )
